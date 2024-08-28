48dd3f98fb9970d85089b6f4c8b2cffbd142f802
humayun2000444 <humayun200044@diit.edu.bd>
2024-08-28 14:20:16 +0600

    WEBRTC HOME DONE

--- frontend/src/views/dashboard/WebRtc/WebRtcHomePage.jsx
+++ frontend/src/views/dashboard/WebRtc/WebRtcHomePage.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import '../../../assets/scss/pages/WebRtcHomePage.scss';
-import GoodMorning from './static/img/GoodMornig.png';
-
+import GoodMorning from './static/img/goodMornig.png';
 import Noon from './static/img/noon.png';
+import Afternoon from './static/img/afternoon.png';
+import GoodNight from './static/img/goodnight.png';
 
 const WebRtcHomePage = () => {
   const [greeting, setGreeting] = useState('');
@@ -17,19 +18,19 @@ const WebRtcHomePage = () => {
 
       if (currentTime >= 5 && currentTime < 12) {
         greetingMessage = 'Good Morning';
-        imageUrl = '/images/morning.jpg'; // Replace with actual image path
+        imageUrl = GoodMorning; // Replace with actual image path
       } else if (currentTime >= 12 && currentTime < 14) {
         greetingMessage = 'Good Noon';
         imageUrl = Noon; // Replace with actual image path
       } else if (currentTime >= 14 && currentTime < 18) {
         greetingMessage = 'Good Afternoon';
-        imageUrl = '/images/afternoon.jpg'; // Replace with actual image path
+        imageUrl = Afternoon; // Replace with actual image path
       } else if (currentTime >= 18 && currentTime < 22) {
         greetingMessage = 'Good Evening';
         imageUrl = '/images/evening.jpg'; // Replace with actual image path
       } else {
         greetingMessage = 'Good Night';
-        imageUrl = '/images/night.jpg'; // Replace with actual image path
+        imageUrl = GoodNight; // Replace with actual image path
       }
 
       setGreeting(greetingMessage);
